cb6f1db599cc20e74cfb25c720d82c62024d874a
Fry <fry1026@gmail.com>
2016-11-05 09:47:21 +0000

    added k_means example

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem  'decisiontree'
+gem 'k_means'
---
+++ kmeans_demo.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'k_means'
+
+data = [[1,1], [1,2], [1,1], [1000, 1000], [500, 500]]
+kmeans = KMeans.new(data, :centroids => 2)
+puts kmeans.inspect
